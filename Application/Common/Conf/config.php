fd4ab2ae58183be8d0441f760f9a4f3454b65001
fisher <gooodfisher@126.com>
2016-12-29 09:10:30 +0800

    ChangeSQLConfig

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -12,7 +12,7 @@ return array(
 	"DB_HOST"=>"localhost",
 	"DB_NAME"=>"ylou",
 	"DB_USER"=>"root",
-	"DB_PWD"=>"root",
+	"DB_PWD"=>"root9527",
 	"DB_PORT"=>"3306",
 	"DB_PREFIX"=>"",
 	'DB_CHARSET'=>'utf8',
